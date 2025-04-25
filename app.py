db8ef88d9d543167553c58af9fcd083b1f20461b
Thedd91 <edoardo.marangon@gmail.com>
2025-04-25 19:29:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Crypto Strategy Engine", layout="wide")
+st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
 # Home page
 st.title("🏠 Home")
