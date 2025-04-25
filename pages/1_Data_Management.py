dac0f6f479621ae006a6dee9551d5e4df8850393
Thedd91 <edoardo.marangon@gmail.com>
2025-04-25 17:28:01 +0200

    Update 1_Data_Management.py

--- pages/1_Data_Management.py
+++ pages/1_Data_Management.py
@@ -2,9 +2,6 @@ import os
 import streamlit as st
 import pandas as pd
 from sqlalchemy import text
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from fetcher_service.db import get_db_session
 from fetcher_service.fetch import fetch_ohlcv
